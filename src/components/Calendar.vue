64b76d434438710f00275130dc915fc5417330fc
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-17 12:31:29 +0700

    calendar finish

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -9,8 +9,13 @@
       <p class="font-bold text-center p-3 cursor-pointer text-white" v-for="namaHari in hari" v-bind:key="namaHari">{{ namaHari }}</p>
     </section>
     <section class="bg-indigo-100 grid grid-cols-7">
-      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in jumlahHari" v-bind:key="num">{{ num }}</p>
+
+      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in startDay()" v-bind:key="num"></p>
+
+      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in daysInMonth(tahun, nomorBulan)" v-bind:key="num">{{ num }}</p>
     </section>
+    <!-- <h5>{{ daysInMonth(tahun, nomorBulan) }}</h5> -->
+    <h5>{{ startDay() }}</h5>
   </div>  
 </template>
 
@@ -19,14 +24,20 @@ export default {
   data(){
     return{
       hari: ['Sen','Sel','Rab', 'Kam', 'Jum', 'Sab', 'Min'],
+      nomorBulan: new Date().getMonth() + 1,
       bulan: new Date().toLocaleString("default", {month: "long"}),
       tahun: new Date().getFullYear(),
       jumlahHari: null 
     }
   },
-  mounted(){
-    let now = new Date()
-    this.jumlahHari = new Date(now.getFullYear(), now.getMonth()+ 1, 0).getDate() 
-  }
+  methods: {
+    daysInMonth(year, bulan){
+      return this.jumlahHari = new Date(year, bulan, 0).getDate();
+    },
+    startDay(){
+      return new Date(2021, 6 - 1, 0).getDay()
+    }
+  },
+  
 }
 </script>
