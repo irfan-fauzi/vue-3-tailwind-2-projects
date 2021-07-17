e45580b8cbd10863182c98a10b467f86a0595ee6
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-17 20:10:24 +0700

    calendar done

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -11,17 +11,27 @@
     </div>
     <!-- nama nama hari -->
     <section class="grid grid-cols-7 bg-indigo-400 justify-center">
-      <p class="font-bold text-center p-3 cursor-pointer text-white" v-for="namaHari in hari" v-bind:key="namaHari">{{ namaHari }}</p>
+      <p class="font-bold text-center p-3 cursor-pointer text-white"
+      v-for="namaHari in hari"
+      v-bind:key="namaHari">{{ namaHari }}</p>
     </section>
     <!-- / nama hari -->
 
     <section class="bg-indigo-100 grid grid-cols-7">
       <!-- tampilan tanggal yang kosong -->
-      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in startDay()" v-bind:key="num"></p>
+      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" 
+      v-for="num in startDay()" 
+      v-bind:key="num"></p>
       <!-- tampilan tanggal yang kosong -->
 
       <!-- tampilan tanggal -->
-      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in daysInMonth(tahun, nomorBulan)" v-bind:key="num">{{ num }}</p>
+      <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200"
+       v-for="num in daysInMonth()"
+       v-bind:key="num"
+       :class="currentDateClass(num)"
+       >
+       {{ num }}
+       </p>
       <!-- / tampilan tanggal -->
     </section>
 
@@ -32,7 +42,7 @@
       <button class="bg-blue-400 py-1 px-6 text-white hover:bg-blue-700 transition-all" @click="next()">Next</button>
     </section>  
     <!-- end section button -->
-    
+   
   </div>  
 </template>
 
@@ -41,6 +51,7 @@ export default {
 
   data(){
     return{
+      currentDate: new Date().getUTCDate() ,
       hari: ['Sen','Sel','Rab', 'Kam', 'Jum', 'Sab', 'Min'],
       nomorBulan: new Date().getMonth(),
       tahun: new Date().getFullYear(),
@@ -48,14 +59,12 @@ export default {
     }
   },
   methods: {
-    daysInMonth(year, bulan){
-      return this.jumlahHari = new Date(year, bulan + 1, 0).getDate();
+    daysInMonth(){
+      return this.jumlahHari = new Date(this.tahun, this.nomorBulan + 1, 0).getDate();
     },
-
     startDay(){
       return new Date(this.tahun, this.nomorBulan , 0).getDay()
     },
-    
     next(){
       if(this.nomorBulan === 11){
         this.nomorBulan = 0;
@@ -71,6 +80,11 @@ export default {
       } else {
         this.nomorBulan--
       }
+    },
+    currentDateClass(num){
+      const calendarFullDate = new Date(this.tahun, this.nomorBulan, num).toDateString()
+      const currentFullDate = new Date().toDateString()
+      return calendarFullDate === currentFullDate ? 'bg-blue-400' : ''
     }
   },
   computed: {
@@ -78,8 +92,5 @@ export default {
       return new Date(this.tahun ,this.nomorBulan).toLocaleString("default", { month: "long"})
     }
   }
-  
-
-  
 }
 </script>
