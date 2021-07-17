d1d3aeb3083450fb4e2b39c47a9b6be300dc5ee1
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-17 14:52:45 +0700

    bulan by me

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,3 +1,7 @@
+
+
+
+
 <template>
   <div class="m-auto max-w-screen-sm border-2">
     <h2 class="text-center font-bold text-2xl my-7">Vue Calendar</h2>
@@ -5,39 +9,70 @@
       <h3 class="text-2xl m-2 font-bold text-black-600 ">{{ bulan }}</h3>
       <h3 class="text-2xl m-2 font-bold text-black-600 ">{{ tahun }}</h3>
     </div>
+    <!-- nama nama hari -->
     <section class="grid grid-cols-7 bg-indigo-400 justify-center">
       <p class="font-bold text-center p-3 cursor-pointer text-white" v-for="namaHari in hari" v-bind:key="namaHari">{{ namaHari }}</p>
     </section>
+    <!-- / nama hari -->
+
     <section class="bg-indigo-100 grid grid-cols-7">
-
+      <!-- tampilan tanggal yang kosong -->
       <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in startDay()" v-bind:key="num"></p>
+      <!-- tampilan tanggal yang kosong -->
 
+      <!-- tampilan tanggal -->
       <p class="min-w-min p-5 text-center border-2 border-green-50 cursor-pointer hover:bg-indigo-200" v-for="num in daysInMonth(tahun, nomorBulan)" v-bind:key="num">{{ num }}</p>
+      <!-- / tampilan tanggal -->
     </section>
-    <!-- <h5>{{ daysInMonth(tahun, nomorBulan) }}</h5> -->
-    <h5>{{ startDay() }}</h5>
+
+
+    <!-- section button -->
+    <section class="flex justify-between my-5">
+      <button class="bg-blue-400 py-1 px-6 text-white hover:bg-blue-700 transition-all" @click="prev()">Prev</button>
+      <button class="bg-blue-400 py-1 px-6 text-white hover:bg-blue-700 transition-all" @click="next()">Next</button>
+    </section>  
+    <!-- end section button -->
+    <h3>{{ getMonthName(nomorBulan) }}</h3>
   </div>  
 </template>
 
 <script>
 export default {
+
   data(){
     return{
       hari: ['Sen','Sel','Rab', 'Kam', 'Jum', 'Sab', 'Min'],
       nomorBulan: new Date().getMonth() + 1,
-      bulan: new Date().toLocaleString("default", {month: "long"}),
+      bulan: null,
       tahun: new Date().getFullYear(),
-      jumlahHari: null 
+      jumlahHari: null,
     }
   },
   methods: {
     daysInMonth(year, bulan){
       return this.jumlahHari = new Date(year, bulan, 0).getDate();
     },
+
     startDay(){
-      return new Date(2021, 6 - 1, 0).getDay()
+      return new Date(this.tahun, this.nomorBulan - 1, 0).getDay()
+    },
+    getMonthName(index){
+      const month = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+      return this.bulan = month[index - 1]
+    },
+    next(){
+      if(this.nomorBulan <= 12){
+        this.nomorBulan++
+        this.getMonthName() + 1
+      }
+    },
+    prev(){
+      this.nomorBulan--
+      this.getMonthName() - 1
     }
   },
+  
+
   
 }
 </script>
