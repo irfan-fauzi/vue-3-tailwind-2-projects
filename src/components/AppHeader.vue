fceccc520d0e2e654616e545fa2189ef6a0897de
irfan-fauzi <irfanwintech@yahoo.com>
2021-07-18 22:01:08 +0700

    loop the path

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,28 +2,37 @@
 
 <template>
   <div class="bg-blue-600 py-5">
-    
-    <div class="flex justify-center gap-5">
-      <router-link to="/">
-        <h1 class="text-yellow-400 font-bold text-xl hover:text-yellow-400">Home</h1>
-      </router-link>
-      <router-link to="/todo">
-        <h1 class="text-white font-bold text-xl hover:text-yellow-400">Todo list</h1>
-      </router-link>
-      <router-link to="/calendar">
-        <h1 class="text-white font-bold text-xl hover:text-yellow-400">Calendar</h1>
-      </router-link>
-       <router-link to="/markdown">
-        <h1 class="text-white font-bold text-xl hover:text-yellow-400">Markdown</h1>
+    <div class="flex justify-center gap-5" >
+      <router-link v-for="item in links" v-bind:key="item.name" v-bind:to="item.path">
+        <h1 class="text-yellow-400 font-bold text-xl hover:text-yellow-400">{{ item.name }}</h1>
       </router-link>
     </div>
-    
   </div>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return {
+      links: [{
+        path: '/',
+        name: 'Home'
+      },
+      {
+        path: '/calendar',
+        name: 'Calendar'
+      }, 
+      {
+        path: '/todo',
+        name: 'Todo App'
+      },
+      {
+        path: '/markdown',
+        name: 'MarkDown'
+      }
+      ]
+    }
+  }
 }
 </script>
 
